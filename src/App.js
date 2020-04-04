f0d8cb507f34e27b5649a40b403778ff1333a66b
Juan Garcia <jgarc210@gmail.com>
2020-04-04 17:02:07 -0500

    'x'

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-import { connect, createLocalVideoTrack } from 'twilio-video'
-var accessToken = require('twilio').jwt.AccessToken;
-const accountSid = "AC8e3ad87b2b2f267fa46b49d4e8c05bc6";
-const authToken = "1328e0b9effc43bf3fa5e63ed03514fe"
-const client = require('twilio')(accountSid, authToken)
+import Room from './Room'
 
 function App() {
-  var [name, setName] = useState({})
-  
-  const accountSid = "AC8e3ad87b2b2f267fa46b49d4e8c05bc6";
-  const apiKey = "SKd9d5c9349104027eb3c74e8b335eab75";
-  const apiSecret = "k3Rlor1aPphRAWxNJeBlxgqO1r7QPLAx";
-  
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-  
-  }
-  
   return (
-    <div className="App">
-      <input type="name" placeholder="name" value={name} onChange={e => setName(e.target.value)} >
-      </input>
-      <button onClick={handleSubmit}>Join class</button>
-      <button onClick={searchRooms()}>Find class</button>
-      <div id="local-media">
-
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={App}></Route>
+        <Route path="/connect/video" component={Room}></Route>
       </div>
-    </div>
+    </Router>
   );
 }
 
