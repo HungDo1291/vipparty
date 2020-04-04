54724fd8762edeb6640ffe800c28963f7e605aa8
Juan Garcia <jgarc210@gmail.com>
2020-04-04 15:41:26 -0500

    'x'

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Video, { connect, createLocalVideoTrack } from 'twilio-video'
-import { AccessToken } from 'twilio';
+import { connect, createLocalVideoTrack } from 'twilio-video'
 var accessToken = require('twilio').jwt.AccessToken;
+const accountSid = "AC8e3ad87b2b2f267fa46b49d4e8c05bc6";
+const authToken = "1328e0b9effc43bf3fa5e63ed03514fe"
+const client = require('twilio')(accountSid, authToken)
 
 function App() {
   var [name, setName] = useState({})
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log("changed name to " + name)
-    const tokenKey = new accessToken(accountSid, apiKey, apiSecret);
-    tokenKey.addGrant(videoGrant)
-    tokenKey.identity = identity
-    var token = tokenKey.toJwt();
-    console.log(tokenKey.toJwt())
-    createVideo()
-    connectVideo(token)
+    
   }
-
-  //create access token for user
-  const VideoGrant = accessToken.VideoGrant;
 
   const accountSid = "AC8e3ad87b2b2f267fa46b49d4e8c05bc6";
   const apiKey = "SKd9d5c9349104027eb3c74e8b335eab75";
   const apiSecret = "k3Rlor1aPphRAWxNJeBlxgqO1r7QPLAx";
-
-  const identity = name
-
-  const videoGrant = new VideoGrant({
-    room: "example-room"
-  })
-
-  function createVideo() {
-    createLocalVideoTrack().then(track => {
-      const localMediaContainer = document.getElementById('local-media');
-      localMediaContainer.appendChild(track.attach())
-    })
-  }
-  
-  function connectVideo(token) {
-    connect(token, { name: "example-class"}).then(room => {
-      console.log("User has joined existing class " + room.sid)
-    })
-  }
 
   return (
     <div className="App">
       <input type="name" placeholder="name" value={name} onChange={e => setName(e.target.value)} >
       </input>
       <button onClick={handleSubmit}>Join class</button>
+      <button onClick={searchRooms()}>Find class</button>
       <div id="local-media">
 
       </div>
